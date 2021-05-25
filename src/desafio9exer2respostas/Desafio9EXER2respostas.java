612049c873609088bf7ec2be11c17b55cb196172
felipe Sampaio <samlipe2006@gmail.com>
2021-05-25 00:43:24 -0300

    desafio9 feito dia 25/05/2021

---
+++ src/desafio9exer2respostas/Desafio9EXER2respostas.java
@@ -0,0 +1,18 @@
+package desafio9exer2respostas;
+/**
+ *
+ * @author Daniela
+ */
+public class Desafio9EXER2respostas {
+    public static void main(String[] args) {
+        int cont;
+        cont = 100;
+        while(cont>=0){
+            if(cont == 0){
+                System.out.println(cont);
+            }
+        }
+        cont--;
+    }
+    
+}
